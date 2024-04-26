ed71af7bf4c08ccc56d6483de83fa9f75e7fd287
Tsalsabilla <tsalsabilla569@gmail.com>
2024-04-26 09:12:57 +0700

    cors

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const DashboardRouter = require("./routes/Dashboard.Route");
 
 app.use(express.text());
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 // app.use(cors({
 //   "origin": ["http://localhost:5000", "http://localhost:5173"],  "methods": ["POST", "GET",  "PUT", "DELETE"],
 //   "credentials": true
