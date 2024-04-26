be9e14fb5a9b1464c7e71ce39413ff620b0c1ded
Tsalsabilla <tsalsabilla569@gmail.com>
2024-04-26 17:55:07 +0700

    no cors

--- index.js
+++ index.js
@@ -40,13 +40,13 @@ app.use(express.json());
 //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 //   credentials: true
 // }));
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://lms-studee-client-production.up.railway.app");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "https://lms-studee-client-production.up.railway.app");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   next();
+// });
 
 //routes
 app.get("/", (req, res) => {
