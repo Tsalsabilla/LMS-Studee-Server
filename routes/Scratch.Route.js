c2244caf85108aa2750b19f426b89d4f1e36e8a7
Tsalsabilla <tsalsabilla569@gmail.com>
2024-04-27 21:40:15 +0700

    activity

--- routes/Scratch.Route.js
+++ routes/Scratch.Route.js
@@ -17,12 +17,36 @@ router.get("/all", async (req, res) => {
   }
 });
 
+//get single scratch
+router.get("/:scratchId", async (req, res) => {
+  const { scratchId } = req.params;
+  try {
+    const scratch = await ScratchModel.find({ _id: scratchId });
+    res.send({ msg: "Single scratch data", scratch: scratch[0] });
+  } catch (error) {
+    res.status(400).send({ msg: "Something went wrong" });
+  }
+});
+
 //create new scratch
 router.post("/create", isAuthenticated, async (req, res) => {
   try {
     const scratch = new ScratchModel(req.body.data);
     await scratch.save();
     return res.send({ msg: "Scratch Created", scratch });
+  } catch (error) {
+    res.status(404).send({ msg: "Error" });
+  }
+});
+
+// add response to scratchs
+router.post("/add", async (req, res) => {
+  try {
+    const scratch = await ScratchModel.findById(req.body.id);
+    scratch.response.push(req.body.desc);
+    await scratch.save();
+    let updatedScratch = await ScratchModel.findById(req.body.id);
+    return res.send({ msg: "Response added", scratch: updatedScratch });
   } catch (error) {
     res.status(404).send({ msg: "Error" });
   }
